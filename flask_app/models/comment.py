8b2b21da76dd220788cb0e7a66a28aa0bec0c3e8
Peter Bui <peter.bui029@gmail.com>
2022-09-25 12:01:56 -0400

    Completed Model files

--- flask_app/models/comment.py
+++ flask_app/models/comment.py
@@ -1,2 +1,42 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
+
+db = 'ghost_sighting_schema'
+
+class Comment:
+    def __init__(self, data):
+        self.content = data['content']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def add_comment(cls):
+        query = "INSERT INTO comments (content) VALUES (%(content)s)"
+        results = connectToMySQL(db).query_db(query)
+        return results
+
+    @classmethod
+    def get_comment_by_sighting_and_user(cls, data):
+        query = "SELECT * FROM comments JOIN sightings ON sightings.id=comments.sighting_id JOIN users ON users.id=comments.user_id WHERE comments.id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def update_comment(cls, data):
+        query = "UPDATE comments SET content=%(content)s WHERE id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def delete_comment(cls, data):
+        query = "DELETE FROM comments WHERE id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @staticmethod
+    def validate_comment(comment):
+        is_valid = True
+        if len(comment['content']) < 3:
+            flash("Content of the comment must be at least 3 characters long")
+            is_valid = False
+        return is_valid
--- flask_app/models/sighting.py
+++ flask_app/models/sighting.py
@@ -1,2 +1,76 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
+
+db = 'ghost_sighting_schema'
+
+class Sighting:
+    def __init__(self, data):
+        self.id = data['id']
+        self.location = data['location']
+        self.date = data['date']
+        self.time = data['time']
+        self.description = data['description']
+        self.intensity = data['intensity']
+        self.num_of_activities = data['num_of_activities']
+        self.reaction = data['reaction']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def add_sighting(cls, data):
+        query = "INSERT into sightings (location, date, time, description, intensity, num_of_activities, reaction, user_id) VALUES (%(location)s, %(date)s, %(time)s, %(description)s, %(intensity)s, %(num_of_activities)s, %(reaction)s, %(user_id)s)"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def update_sighting(cls, data):
+        query = "UPDATE sightings SET location=%(location)s, date=%(date)s, time=%(time)s, description=%(description)s, intensity=%(intensity)s, num_of_activities=%(num_of_activities)s, reaction=%(reaction)s id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def get_one_sighting_by_user(cls, data): #View Sighting Page
+        query = "SELECT * FROM sightings JOIN users ON users.id=sightings.user_id WHERE sightings.id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def get_all_sightings_by_users(cls, data): #Main Page
+        query = "SELECT * FROM sightings JOIN users ON users.id=sightings.user_id"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def get_all_sightings_by_user(cls, data): #User Page
+        query = "SELECT * FROM sightings JOIN users ON users.id=sightings.user_id WHERE users.id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @classmethod
+    def delete_sighting(cls, data):
+        query = "DELETE FROM sightings WHERE id=%(id)s"
+        results = connectToMySQL(db).query_db(query, data)
+        return results
+
+    @staticmethod
+    def validate_sighting(sighting):
+        is_valid = True
+        if len(sighting['location']) < 3:
+            flash("Location must be at least 3 characters long")
+            is_valid = False
+        if not sighting['date']:
+            flash("Date must be filled in")
+            is_valid = False
+        if not sighting['time']:
+            flash("Time must be filled in")
+            is_valid = False
+        if len(sighting['description']) < 3:
+            flash("Description must be at least 3 characters long")
+            is_valid = False
+        if sighting['num_of_activities'] < 1:
+            flash("Number of Activities must be at least 1")
+            is_valid = False
+        if len(sighting['reaction']) < 3:
+            flash("Reaction must be at least 3 characters long")
+            is_valid = False
+        return is_valid
